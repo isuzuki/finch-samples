40e57d0afea54f72a62e8dfd9cc3d903c17c58af
isuzuki <ikkuntech@gmail.com>
2018-11-19 16:41:34 +0900

    added post endpoint sample

--- src/main/scala/io/github/isuzuki/SampleApp.scala
+++ src/main/scala/io/github/isuzuki/SampleApp.scala
@@ -4,6 +4,7 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Await
 import io.finch._
+// Note: this import is required to build.
 import io.finch.circe._
 import io.finch.syntax._
 import io.finch.Endpoint
@@ -13,14 +14,20 @@ object SampleApp extends App {
 }
 
 object Router {
-  val hello: Endpoint[String] = get("hello") {
-    Ok("hello finch.")
+  val helloGet: Endpoint[String] = get("hello") {
+    Ok("hello get finch.")
   }
 
-  val service: Service[Request, Response] = hello.handle {
-    case e: Exception => {
-      e.printStackTrace()
-      BadRequest(e)
-    }
-  }.toService
+  val helloPost: Endpoint[String] = post("hello" :: jsonBody[String]) { s: String =>
+    Ok(s"hello post finch: $s.")
+  }
+
+  val service: Service[Request, Response] =
+    (helloGet :+: helloPost)
+      .handle {
+      case e: Exception => {
+        e.printStackTrace()
+        BadRequest(e)
+      }
+    }.toService
 }
